b61ab1ec1b97b7f0ee05986b09ffcfa3fa89f17e
Diana Murtazina <murka8831@yandex.ru>
2022-11-17 19:21:02 +0400

    establishing a database connection

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const config = require("config");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = config.get("port") || 5000;
 
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+async function start() {
+    try {
+        await mongoose.connect(config.get("mongoURL"), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+    } catch (error) {
+        console.log("Server error", error.message);
+        process.exit(1);
+    }
+}
+
+start();
