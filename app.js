865624961db61c889d246077582f46a5f51bf6f8
Diana Murtazina <murka8831@yandex.ru>
2022-11-26 15:38:27 +0400

    preparation for publication attempt 5

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ app.use(express.json({ extended: true }));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/user.routes"));
 
-if (process.env.NODE_ENV === "production") {
-    app.use("/", express.static(path.join(__dirname, "client", "build")));
+// if (process.env.NODE_ENV === "production") {
+//     app.use("/", express.static(path.join(__dirname, "client", "build")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//     });
+// }
 
 async function start() {
     try {
